c6fcb5c3dab3f35c710669b14b1b6c0ca25d3d25
champix <champix@gmail.com>
2018-04-11 08:10:30 +0200

    three.js geometry text hello world (with X camera rotation)

--- js/3dSpTree/sptree.js
+++ js/3dSpTree/sptree.js
@@ -1,33 +1,118 @@
-var fontLoader = new THREE.FontLoader();
-var canalFont = fontLoader.load('C+BoldItal_Regular.json', function(canalFont) {
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var cameraTarget;
+var group;
+var mesh;
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var targetRotation = 0;
+var targetRotationOnMouseDown = 0;
+var mouseX = 0;
+var mouseXOnMouseDown = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+window.addEventListener('resize', function() {
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+	
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	
+	renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-var scene = new THREE.Scene();
-	var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-	var renderer = new THREE.WebGLRenderer();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	document.body.appendChild(renderer.domElement);
-
-	window.addEventListener('resize', function() {
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
+var fontLoader = new THREE.FontLoader();
+fontLoader.load('C+BoldItal_Regular.json', function(canalFont) {
+	
+	document.addEventListener('mousedown', onDocumentMouseDown, false);
+	document.addEventListener('touchstart', onDocumentTouchStart, false);
+	document.addEventListener('touchmove', onDocumentTouchMove, false);
+	
+	group = new THREE.Group();
+	group.position.y = 100;
+	scene.add(group);
+	var textGeo = new THREE.TextGeometry('Hello world', {
+		font: canalFont,
+		size: 100,
+		height: 50,
+		curveSegments: 12,
+		bevelEnabled: false,
+		bevelThickness: 10,
+		bevelSize: 8,
+		bevelSegments: 3
 	});
+	textGeo.computeBoundingBox();
+	textGeo.computeVertexNormals();
+	var centerOffset = -0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
+	
+	//var geometry = new THREE.BoxGeometry(1, 1, 1);
+	
+	var material = new THREE.MeshBasicMaterial({color: 0xffffff});
+	mesh = new THREE.Mesh(textGeo, material);
+	mesh.position.x = centerOffset;
+	mesh.position.y = 30;
+	mesh.position.z = 0;
+	mesh.rotation.x = 0;
+	mesh.rotation.y = Math.PI * 2;
+	group.add(mesh);
 
-	var geometry = new THREE.BoxGeometry(1, 1, 1);
-	var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-	var cube = new THREE.Mesh(geometry, material);
-	scene.add(cube);
-
-	camera.position.z = 5;
-
-	function animate() {
-		requestAnimationFrame(animate);
-		
-		//cube.rotation.x += 0.1;
-		//cube.rotation.y += 0.1;
-		
-		renderer.render(scene,camera );
-	}
+	camera.position.z = 450;
+	cameraTarget = new THREE.Vector3(0, 150, 0);
 	animate();
+});
+
+function onDocumentMouseDown( event ) {
+	event.preventDefault();
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+	document.addEventListener( 'mouseout', onDocumentMouseOut, false );
+	mouseXOnMouseDown = event.clientX - windowHalfX;
+	targetRotationOnMouseDown = targetRotation;
+}
+function onDocumentMouseMove( event ) {
+	mouseX = event.clientX - windowHalfX;
+	targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
+}
+function onDocumentMouseUp( event ) {
+	document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+	document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+}
+function onDocumentMouseOut( event ) {
+	document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+	document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+}
+function onDocumentTouchStart( event ) {
+	if ( event.touches.length == 1 ) {
+		event.preventDefault();
+		mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
+		targetRotationOnMouseDown = targetRotation;
+	}
+}
+function onDocumentTouchMove( event ) {
+	if ( event.touches.length == 1 ) {
+		event.preventDefault();
+		mouseX = event.touches[ 0 ].pageX - windowHalfX;
+		targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
+	}
+}
+
+
+
+
+function animate() {
+	requestAnimationFrame(animate);
+	render();
+}
+
+function render() {
+	group.rotation.y += (targetRotation - group.rotation.y) * 0.05;
+	camera.lookAt(cameraTarget);
+	renderer.clear();
+	renderer.render(scene, camera);
+}
